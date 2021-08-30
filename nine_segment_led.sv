4189f8dd3983f34250e45cc9bd2e941861efd0e0
Masato Hori <rillomas@gmail.com>
2021-08-30 22:41:41 +0900

    refactored interface

--- nine_segment_led.sv
+++ nine_segment_led.sv
@@ -6,7 +6,15 @@ module nine_segment_led(
 	assign s = {s2,s1,s0};
 	eight_dice dice(.s(s), .out(out));
 	// convert 9 segment signals to 6 pin anode LED pins
-	logic [5:0] pins;
-	nine_segment_to_six_pin convert(.segments(out), .pins(pins));
-	assign {o0,o1,o2,o3,o4,o5} = pins;
+	logic [2:0] rows;
+	logic [2:0] cols;
+	nine_segment_to_six_pin convert(.segments(out), .rows(rows), .cols(cols));
+	// o0 ... row 1 (+)
+	// o1 ... row 2 (+)
+	// o2 ... row 3 (+)
+	// o3 ... col 1 (-)
+	// o4 ... col 2 (-)
+	// o5 ... col 3 (-)
+	assign {o0,o1,o2} = rows;
+	assign {o3,o4,o5} = cols;
 endmodule
--- nine_segment_to_six_pin.sv
+++ nine_segment_to_six_pin.sv
@@ -1,5 +1,6 @@
 // Convert a nine segment signal to a 3x3 anode common LED pins (6 pins)
 module nine_segment_to_six_pin(
 	input logic [8:0] segments,
-	output logic [5:0] pins);
+	output logic [2:0] rows,
+	output logic [2:0] cols);
 endmodule

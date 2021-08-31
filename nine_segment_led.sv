8c5b4d4c630eebf6490b8c9bf6d52d4f3fd752b5
Masato Hori <rillomas@gmail.com>
2021-08-31 21:21:13 +0900

    added experimental code

--- nine_segment_led.sv
+++ nine_segment_led.sv
@@ -1,6 +1,13 @@
 module nine_segment_led(
-	input logic s0,s1,s2,
+	input logic clk,s0,s1,s2,
 	output logic o0,o1,o2,o3,o4,o5);
+	logic [31:0] cnt;
+	initial begin
+		cnt = 32'h00000000; // initialize counter with zero
+	end
+	always_ff @(posedge clk) begin
+		cnt <= cnt + 1; // count up
+	end
 	logic [2:0] s;
 	logic [8:0] out;
 	assign s = {s2,s1,s0};
@@ -8,7 +15,9 @@ module nine_segment_led(
 	// convert 9 segment signals to 6 pin anode LED pins
 	logic [2:0] rows;
 	logic [2:0] cols;
-	nine_segment_to_six_pin convert(.segments(out), .rows(rows), .cols(cols));
+	logic led;
+	assign led = cnt[24];
+	nine_segment_to_six_pin convert(.clk(led), .segments(out), .rows(rows), .cols(cols));
 	// o0 ... row 1 (+)
 	// o1 ... row 2 (+)
 	// o2 ... row 3 (+)
--- nine_segment_to_six_pin.sv
+++ nine_segment_to_six_pin.sv
@@ -1,6 +1,49 @@
 // Convert a nine segment signal to a 3x3 anode common LED pins (6 pins)
 module nine_segment_to_six_pin(
+	input logic clk,
 	input logic [8:0] segments,
 	output logic [2:0] rows,
 	output logic [2:0] cols);
+	typedef enum logic [1:0] {R0, R1, R2} row_index;
+	row_index state, next_state;
+	initial begin
+		state = R0;
+	end
+	always_ff @(posedge clk) begin
+		// Switch between rows based on clock value to simultaneously light multiple rows
+		state <= next_state;
+	end
+
+	always_comb begin
+		// light LED and transition to next state
+		case (state)
+			R0: begin
+				rows[2] = segments[8] | segments[7] | segments[6];
+				rows[1] = 0;
+				rows[0] = 0;
+				next_state = R1;
+			end
+			R1: begin
+				rows[2] = 0;
+				rows[1] = segments[5] | segments[4] | segments[3];
+				rows[0] = 0;
+				next_state = R2;
+			end
+			R2: begin
+				rows[2] = 0;
+				rows[1] = 0;
+				rows[0] = segments[2] | segments[1] | segments[0];
+				next_state = R0;
+			end
+			default: begin
+				rows[2] = 0;
+				rows[1] = 0;
+				rows[0] = 0;
+				next_state = R0;
+			end
+		endcase
+	end
+	assign cols[2] = ~(segments[8] | segments[5] | segments[2]);
+	assign cols[1] = ~(segments[7] | segments[4] | segments[1]);
+	assign cols[0] = ~(segments[6] | segments[3] | segments[0]);
 endmodule
